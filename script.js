47a89b89e73f431b85b6263cbff697402f224b5c
Arram Rishika <135079553+arramrishika2007@users.noreply.github.com>
2026-07-04 19:10:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = `List Item ${count++}`;
+    list.appendChild(li);
+  }
+}
+
+// Initial 10 items
+addItems(10);
+
+// Infinite scrolling
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
